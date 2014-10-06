4c5d63fb2a45c8f4825a33226953d60400910079
darianmacdonald <darianmacdonald@me.com>
2014-10-06 15:36:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 script.js
+
+document.addEventListener("keyup",function(event){userdata(event);},false);
+
+function userdata(event) {
+  var userInput = event;
+
+  if (userInput.keyCode === 13) { 
+    var msg = document.getElementById("text_box").value;
+  console.log(msg);
+  var x = document.createElement("P");
+  var t = document.createTextNode(msg);
+  x.appendChild(t);
+  document.getElementById("msg_area").appendChild(x);
+  document.getElementById("text_box").value = "";
+  }
+
+}
